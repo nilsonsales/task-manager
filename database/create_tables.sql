dc3fdf57b5b8b63e520917666edc71273c6ef380
Nilson Sales <nilsonsaless@gmail.com>
2023-11-12 11:54:36 -0300

    Update SQL script and secrets

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -2,7 +2,7 @@ CREATE DATABASE task_manager;
 
 CREATE SCHEMA task_manager;
 
-CREATE TABLE tasks (
+CREATE TABLE tasks_manager.tasks (
 	id BIGSERIAL PRIMARY KEY,
     task_name VARCHAR(255),
     task_description TEXT,
