4a6dd0d29d72fd6c7601380218bbf66d1f8c99b0
Nilson Sales <nilsonsaless@gmail.com>
2023-11-12 00:22:01 -0300

    Add script for DB creation

---
+++ database/create_tables.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE task_manager;
+
+CREATE SCHEMA task_manager;
+
+CREATE TABLE tasks (
+	id BIGSERIAL PRIMARY KEY,
+    task_name VARCHAR(255),
+    task_description TEXT,
+    due_date DATE,
+    priority INTEGER,
+    is_completed BOOLEAN,
+    created_at TIMESTAMP WITH time ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH time ZONE DEFAULT NULL
+);
